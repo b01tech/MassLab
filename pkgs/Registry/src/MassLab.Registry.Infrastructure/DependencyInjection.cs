7bd972ec593c42064409845fbd0107a196e903a7
b01tech <bruno01.tech@gmail.com>
2026-03-10 17:25:08 -0300

    refactor: remove unused IConfiguration parameters from infrastructure DI

--- pkgs/Registry/src/MassLab.Registry.Infrastructure/DependencyInjection.cs
+++ pkgs/Registry/src/MassLab.Registry.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,12 @@
 using MassLab.Registry.Domain.Interfaces;
 using MassLab.Registry.Infrastructure.Persistence.Repositories;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MassLab.Registry.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddRegistryInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddRegistryInfrastructure(this IServiceCollection services)
     {
         services.AddScoped<IOwnerRepository, OwnerRepository>();
         services.AddScoped<IClientRepository, ClientRepository>();
